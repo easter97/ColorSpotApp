dde679988684f67133f61a0728e1999aa19065bc
andermoran <andermoran@me.com>
2019-05-03 17:46:01 -0500

    Got it to read color... but not the right color :(
    Got it to read color... but not the right color :(

--- ColorSpot App/ColorSpot App/ViewController.swift
+++ ColorSpot App/ColorSpot App/ViewController.swift
@@ -8,15 +8,65 @@
 
 import UIKit
 
+//On the top of your swift
+extension UIImage {
+    func getPixelColor(pos: CGPoint) -> UIColor {
+        let provider = self.cgImage!.dataProvider
+        let providerData = provider!.data
+        let pixelData = providerData!
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        
+        let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+        
+        let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+        let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+        let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+        let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+}
+
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     var imagePicker: UIImagePickerController!
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var debugBox: UIView!
+    /// Takes the screenshot of the screen and returns the corresponding image
+    ///
+    /// - Parameter shouldSave: Boolean flag asking if the image needs to be saved to user's photo library. Default set to 'true'
+    /// - Returns: (Optional)image captured as a screenshot
+    open func takeScreenshot(_ shouldSave: Bool = true) -> UIImage? {
+        var screenshotImage :UIImage?
+        let layer = UIApplication.shared.keyWindow!.layer
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+        guard let context = UIGraphicsGetCurrentContext() else {return nil}
+        layer.render(in:context)
+        screenshotImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        if let image = screenshotImage, shouldSave {
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
+        return screenshotImage
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let position = touch.location(in: view)
+            print(position)
+            let screenshot = takeScreenshot(true)
+            let color = screenshot!.getPixelColor(pos: position)
+            print(color)
+            debugBox.backgroundColor = color
+        }
+    }
 
     @IBAction func takePhoto(_ sender: Any) {
         imagePicker =  UIImagePickerController()
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         
         present(imagePicker, animated: true, completion: nil)
     }
