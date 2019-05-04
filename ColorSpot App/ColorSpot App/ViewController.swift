ead88ee563c554b56b13606f60ce634d562c2613
Amanda Easter <easter97@tamu.edu>
2019-05-03 19:19:36 -0500

    Print colors to console

--- ColorSpot App/ColorSpot App/ViewController.swift
+++ ColorSpot App/ColorSpot App/ViewController.swift
@@ -63,6 +63,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             let color = imageView.image!.getPixelColor(atLocation: position, withFrameSize: imageView.frame.size)
             print(color)
             debugBox.backgroundColor = color
+            print(getColorName(color))
         }
     }
 
@@ -84,6 +85,64 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imagePicker.dismiss(animated: true, completion: nil)
         imageView.image = info[.originalImage] as? UIImage
+    }
+    func getColorName(_ color: UIColor) -> String
+    {
+        var colorName :String = "White"
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        var hue: CGFloat = 0
+        var saturation: CGFloat = 0
+        var brightness: CGFloat = 0
+        
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        color.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+        
+        let rgDif: CGFloat = abs(red - green) * 256
+        let gbDif: CGFloat = abs(green - blue) * 256
+        let brDif: CGFloat = abs(blue - red) * 256
+        
+        if (hue == 0 || saturation <= 0.01 ||
+            (rgDif < 15 && gbDif < 15 && brDif < 15)
+            ) {
+            let redValue: CGFloat = red * 256;
+            if (redValue < 22) {
+                colorName = "Black";
+            } else if (redValue >= 22 && redValue < 241) {
+                colorName = "Gray";
+            } else {
+                colorName = "White";
+            }
+        } else if (0 < hue && hue <= 18) {
+            colorName = "Red";
+        } else if (19 <= hue && hue <= 42) {
+            colorName = "Orange";
+        } else if (42 <= hue && hue <= 70) {
+            colorName = "Yellow";
+        } else if (71 <= hue && hue <= 79) {
+            colorName = "Lime";
+        } else if (80 <= hue && hue <= 163) {
+            colorName = "Green";
+        } else if (164 <= hue && hue <= 193) {
+            colorName = "Cyan";
+        } else if (194 <= hue && hue <= 240) {
+            colorName = "Blue";
+        } else if (241 <= hue && hue <= 260) {
+            colorName = "Indigo";
+        } else if (261 <= hue && hue <= 270) {
+            colorName = "Violet";
+        } else if (271 <= hue && hue <= 291) {
+            colorName = "Purple";
+        } else if (292 <= hue && hue <= 327) {
+            colorName = "Magenta";
+        } else if (328 <= hue && hue <= 344) {
+            colorName = "Rose";
+        } else if (345 <= hue && hue <= 360) {
+            colorName = "Red";
+        }
+        return colorName;
     }
     
 }
